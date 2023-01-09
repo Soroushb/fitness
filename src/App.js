a241f1758d2ea7ea3c4ebaceb8b47aca31b89563
Soroush Bahrami <sbahrami41@yahoo.com>
2023-01-09 11:48:24 -0500

    Footer Added

--- src/App.js
+++ src/App.js
@@ -28,8 +28,18 @@ const App = () => {
                     </Routes>
                 </div>
             </Layout>
+        <div className='footer' level={5} style={{color:'white', textAlign:'center'}}>
+            <Typography.Title>
+                CryptoVerse<br/>
+                All Rights Reserved.
+            </Typography.Title>
+            <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+            </Space>
         </div>
-        <div className='footer'></div>
+    </div>
     </div>
   )
 }
