c50b8baef8dba90038ef29c101a3d9b12c953497
Soroush Bahrami <sbahrami41@yahoo.com>
2023-01-10 21:42:31 -0500

    Finished

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -4,7 +4,9 @@ import { useParams } from 'react-router-dom'
 import millify from 'millify'
 import { Col, Row, Typography, Select } from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
-import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+import LineChart from './LineChart';
 const {Title, Text} = Typography;
 const { Option } = Select;
 
@@ -16,7 +18,10 @@ const CryptoDetails = () => {
   const {coinId} = useParams();
   const {timePeriod, setTimePeriod} = useState('7d')
   const {data, isFetching} = useGetCryptoDetailsQuery(coinId);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod });
   const cryptoDetails = data?.data?.coin;
+
+  if (isFetching) return <Loader />;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
