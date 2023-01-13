dfc9367546b56e118f49768b1961e3be4c4bb18d
Soroush Bahrami <sbahrami41@yahoo.com>
2023-01-12 21:04:46 -0500

    Pagination Improved

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
-import {Card, Row, Col, Input, Select, Typography, Button} from 'antd';
+import {Card, Space, Row, Col, Input, Select, Typography, Button, Pagination } from 'antd';
 import { useGetGenresQuery, useGetMoviesQuery, useGetMoviesByDecadeQuery } from '../services/MoviesApi';
 
 
 const Movies = () => {
 
     const {Option} = Select
+    const {Title} = Typography;
     const { data } = useGetGenresQuery();
     const decades = [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020]
     //console.log(data)
@@ -18,6 +19,9 @@ const Movies = () => {
     const titles = movies?.results
     const {data: moviesByDecade} = useGetMoviesByDecadeQuery({startYear: decade,page: page, endYear: decade+9});
 
+    const onChange = (page) => {
+      setPage(page);
+    };
     //console.log(titles)
    //{titles?.map((title, index) => <Option key={index} value={title?.id}>{title?.id}</Option>)}
 
@@ -25,26 +29,47 @@ const Movies = () => {
 
   return (
 
+    <>
+    <Row gutter={[24,24]}>
+    <Col span={8}>
+    <Typography>Favorite Era</Typography>
+    <Select showSearch className='select-news' placeholder='Favorite Decade' optionFilterProp='children' onChange={(value) => setDecade(value)}  filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0} >
+      {decades?.map((decade, index) => <Option key={index} value={decade}>{decade}</Option>)}
+    </Select>
+    </Col>
+    <Col span={8} offset={8}>
+    <Space size={20}>
+    <Row>
+    <Pagination page={page} onChange={onChange} total={500} />
+    </Row>
+    </Space>
+    </Col> 
+    </Row>
+
     <Row gutter={[24,24]}> 
-        <Col span={24}>
-        <Typography>Favorite Era</Typography>
-          <Select showSearch className='select-news' placeholder='Favorite Decade' optionFilterProp='children' onChange={(value) => setDecade(value)}  filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0} >
-            {decades?.map((decade, index) => <Option key={index} value={decade}>{decade}</Option>)}
-          </Select>
-          {console.log(moviesByDecade)}
-          <Col xs={24} sm={12} lg={6} className="crypto-card">
+    
+    {console.log(moviesByDecade)}
+  
+    
           {moviesByDecade?.results?.map((movie) => movie?.titleType?.text === "Movie" ? (
-            <p>{movie?.titleText?.text}</p>
+            <Col xs={24} sm={12} lg={6} className="crypto-card">
+            <Card hoverable className='news-card'>
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>{movie?.titleText?.text}</Title>
+              </div>
+              <p>
+                Release Year: {movie?.releaseYear?.year}
+              </p>
+          </Card>
+          </Col>
           ) : (<></>))}
+        </Row>
           <Row>
           <Button onClick={() => {if(page > 0) setPage(page - 1)}}>Prev Page</Button>
           <p>{page}</p>
           <Button onClick={() => setPage(page + 1)}>Next Page</Button>         
           </Row>
-          </Col>
-        </Col>
-    </Row>
-
+        </>
     )
 }
 
