08edbaf6ac3823664f9d5025c8a6f05bbc0b1136
Soroush Bahrami <sbahrami41@yahoo.com>
2023-12-05 16:09:58 -0500

    burned calorie is shown

--- src/components/Diet.jsx
+++ src/components/Diet.jsx
@@ -13,72 +13,10 @@ const Diet = (props) => {
     const nextColorCls = level >= mid ? 'icon-wrapper-active' : '';
     const {data} = useGetActivitiesQuery(level);
     const [activity, setActivity] = useState("")
-    const [activityMin, setActivityMin] = useState(50)
-    const [weight, setWeight] = useState(75)
-    const [result, setResult] = useState(false)
-
-    const [open, setOpen] = useState(false);
-    const [ResultOpen, setResultOpen] = useState(false);
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const [confirmResultLoading, setConfirmResultLoading] = useState(false);
     const {data: calorie} = useGetBurnedCaloriesQuery(activity)
+    console.log(calorie?.burnedCalorie)
+ 
 
-  const showModal = () => {
-    setOpen(true);
-  };
-
-  const showResultModal = () => {
-    setResultOpen(true);
-  };
-
-  const handleOk = () => {
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
-    setResultOpen(true)
-
-  };
-
-  const handleResultOk = () => {
-    setConfirmResultLoading(true);
-    setTimeout(() => {
-      setResultOpen(false);
-      setResult(true)
-      setConfirmResultLoading(false);
-    }, 2000);
-  };
-
-
-  const onChangeMin = (value) => {
-    setActivityMin(value)
-  };
-
-  const onChangeWeight = (value) => {
-    setWeight(value)
-  };
-
-
-  
-  const handleCancel = () => {
-    console.log('Clicked cancel button');
-    setOpen(false);
-  };
-
-    
-  const handleResultCancel = () => {
-    console.log('Clicked cancel button');
-    setOpen(false);
-  };
-
-  const handleClick = (id) => {
-
-    setActivity(id);
-    showModal()
-
-    
-  }
 
 
     return (
@@ -87,41 +25,25 @@ const Diet = (props) => {
        <div className='slider-container'>
         <div onClick={() => level > 1 ? setLevel(level - 1) : null} className='intensity-button'>-</div>
         <div className="icon-wrapper">
+        <div>Result:{calorie?.data?.burnedCalorie}</div>
+
         <Slider {...props} onChange={setLevel} value={level} />
       </div>
+
       <div onClick={() => level < 9 ? setLevel(level + 1) : null} className='intensity-button'>+</div>
        </div> 
 
       
         <div className='activities-container'>
        {data?.data.slice(0, 20).map((activity) => (
-        <div onClick={() => handleClick(activity?.id)} className='activity-container'>
+        <div onClick={() => setActivity(activity?.id)} className='activity-container'>
               <p>{activity.description}</p>
         </div>
        ))}
        </div>
 
-      <Modal
-        title="Calculater"
-        open={open}
-        onOk={handleOk}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-      >
-      <p>How Many Minutes:</p>
-      <InputNumber min={1} max={10} defaultValue={25} onChange={onChangeMin} />
-      <p>Weight(kg):</p>
-      <InputNumber min={1} max={10} defaultValue={75} onChange={onChangeWeight} />
 
-
-      </Modal>
-
-      {ResultOpen && (
-        <ResultModal activity={activity}/>
-      )}
-     
-       
-
+  
        </>
       
     );
